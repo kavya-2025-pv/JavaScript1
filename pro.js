6a92ce2c1f8d676f9821fbc2ae706348a569ba6d
kavya-2025-pv <kavyapv723@gmail.com>
2026-08-07 16:47:11 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,54 @@
+// console.log("hello, This is external javascript")
+// var name;
+// name="Rama";
+// var name;
+// name="Sita";
+// console.log(name);
+
+
+
+// let Name;
+// Name="Kavya";
+
+// Name="Gowda";
+
+// console.log(Name);
+
+
+// const Nm="Hii";
+// console.log(Nm);
+// function print(){
+//     console.log(name);
+// }
+// print()
+
+
+// {
+//     var AA="hii"
+    
+// }
+// console.log(AA)
+
+// {
+//     let AAA="hii"
+//     console.log(AAA)
+// }
+
+// {
+//     const name="hello";
+//     console.log(name)
+
+// }
+
+function print(){
+    var Name="Kavya"
+    console.log(Name);
+}
+
+print()
+
+function print1(){
+    let nm="hii"
+    console.log("hey"+nm)
+}
+print1()
